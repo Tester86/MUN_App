9dcf1a57eee11a6350ce968f918e658af9552c8b
Tester86 <tester86t@gmail.com>
2020-08-09 17:19:40 +0200

    🔎 Adding empty field control to "Join Us"

--- app/src/main/java/com/delta/leadershipmun/JoinUsActivity.java
+++ app/src/main/java/com/delta/leadershipmun/JoinUsActivity.java
@@ -3,11 +3,13 @@ package com.delta.leadershipmun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Properties;
 
 public class JoinUsActivity extends AppCompatActivity {
 
@@ -32,6 +34,14 @@ public class JoinUsActivity extends AppCompatActivity {
 
     }
 
+    public void toggleOtherEditText(View v){
+        if(munOtherSpecification.getVisibility() == View.INVISIBLE){
+            munOtherSpecification.setVisibility(View.VISIBLE);
+        } else{
+            munOtherSpecification.setVisibility(View.INVISIBLE);
+        }
+    }
+
     public void Submit(View v){
 
         boolean formIsNotComplete = false;
@@ -44,17 +54,44 @@ public class JoinUsActivity extends AppCompatActivity {
         CheckBox [] munConferences = new CheckBox[]{munImpact, munBilbao, royalRussel, other};
 
         for(int i = 0; i < fields.length - 1; i++){
-            if(fields[i].equals("") || !munConferences[i].isChecked()){
+            if(fields[i].equals("")){
                 formIsNotComplete = true;
                 break;
             }
         }
-        if(formIsNotComplete) {
-            Toast.makeText(this, "All fields must be filled", Toast.LENGTH_LONG).show();
-        } else{
-            // send email with data
+
+        for(int i = 0; i < munConferences.length - 1; i++){
+            if (munConferences[i].isChecked()) {
+                formIsNotComplete = false;
+                break;
+            } else{
+                formIsNotComplete = true;
+            }
+
         }
 
 
+        if(formIsNotComplete) {
+            Toast.makeText(this, "All fields must be filled", Toast.LENGTH_LONG).show();
+        } else{
+
+        }
+
+
+    }
+
+    private boolean sendMail(){
+
+        boolean emailSentSuccess = false;
+        String to = "tester86t@gmail.com";
+        String from = "tester86t@gmail.com";
+
+        String host = "localhost";
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+
+        return emailSentSuccess;
     }
 }
