490d5810a7b521a59b4790fa2dbf5c470f84396e
Tester86 <tester86t@gmail.com>
2020-08-15 11:40:12 +0200

    Adding: "Staff" to "About Us" section

--- app/src/main/java/com/delta/leadershipmun/AboutUsActivity.java
+++ app/src/main/java/com/delta/leadershipmun/AboutUsActivity.java
@@ -3,21 +3,51 @@ package com.delta.leadershipmun;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class AboutUsActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
+    private ImageView scroll_tutorial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        setContentView(R.layout.activity_about_us_entry_point);
 
+
+
+    }
+
+    public void viewStaff(View v){
+
+        setContentView(R.layout.activity_about_us);
+        scroll_tutorial = (ImageView)findViewById(R.id.scroll_tutorial);
         viewPager = (ViewPager)findViewById(R.id.viewPager);
         viewPagerAdapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(viewPagerAdapter);
 
+        Animation fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+
+        scroll_tutorial.startAnimation(fadeOut);
+        scroll_tutorial.setVisibility(View.INVISIBLE);
+
+
+    }
+
+    public void viewCoreValues(View v){
+        // Show core values document
     }
 }
